4fc1d8988ff999aa3afb544fcff5432314d9299d
Artem Yurchenko <zumikq78@gmail.com>
2021-10-31 21:23:51 +0100

    RestaurantMenu property was added to the RestaurantData class

--- Pyszne-API/APISystem/RestaurantData.cs
+++ Pyszne-API/APISystem/RestaurantData.cs
@@ -2,6 +2,11 @@ namespace Pyszne_API.APISystem
 {
 	public class RestaurantData
 	{
-		
+		public RestaurantMenu Menu { get; }
+
+		public RestaurantData (RestaurantMenu menu)
+		{
+			Menu = menu;
+		}
 	}
 }
