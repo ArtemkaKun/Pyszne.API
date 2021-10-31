778a87e23bffe1317bebf9d77ab12095b0881521
Artem Yurchenko <zumikq78@gmail.com>
2021-10-31 21:55:02 +0100

    RestaurantData - constructor was chagned on init modifier

--- Pyszne-API/APISystem/RestaurantData.cs
+++ Pyszne-API/APISystem/RestaurantData.cs
@@ -2,11 +2,6 @@ namespace Pyszne_API.APISystem
 {
 	public class RestaurantData
 	{
-		public RestaurantMenu Menu { get; }
-
-		public RestaurantData (RestaurantMenu menu)
-		{
-			Menu = menu;
-		}
+		public RestaurantMenu Menu { get; init; }
 	}
 }
