9feb0add9fd3320df9eb322233b0d2dc39670aed
Artem Yurchenko <zumikq78@gmail.com>
2021-10-31 21:54:37 +0100

    MenuCategories - constructor was changed on init modifiers

--- Pyszne-API/APISystem/MenuCategory.cs
+++ Pyszne-API/APISystem/MenuCategory.cs
@@ -4,17 +4,9 @@ namespace Pyszne_API.APISystem
 {
 	public class MenuCategory
 	{
-		public string ID { get; }
-		public string Name { get; }
-		public string Description { get; }
-		public IReadOnlyList<string> ProductIDs { get; }
-
-		public MenuCategory (string id, string name, string description, IReadOnlyList<string> productIDs)
-		{
-			ID = id;
-			Name = name;
-			Description = description;
-			ProductIDs = productIDs;
-		}
+		public string ID { get; init; }
+		public string Name { get; init; }
+		public string Description { get; init; }
+		public IReadOnlyList<string> ProductIDs { get; init; }
 	}
 }
