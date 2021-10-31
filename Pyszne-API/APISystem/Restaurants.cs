66d58b1e2b6a61355c2ba37b8872daa7c3637e33
Artem Yurchenko <zumikq78@gmail.com>
2021-10-31 21:56:07 +0100

    Restaurant - restaurant data request - headers were added

--- Pyszne-API/APISystem/Restaurants.cs
+++ Pyszne-API/APISystem/Restaurants.cs
@@ -23,6 +23,24 @@ namespace Pyszne_API.APISystem
 		private string MakeRestaurantDataRequest (string restaurantRequest)
 		{
 			HttpWebRequest request = (HttpWebRequest) WebRequest.Create(restaurantRequest);
+			request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+			request.Headers.Add("authority", "cw-api.takeaway.com");
+			request.Headers.Add("x-instana-t", "a72df02c74bb0d7b");
+			request.Headers.Add("x-instana-s", "a72df02c74bb0d7b");
+			request.Headers.Add("accept-language", "pl");
+			request.Headers.Add("sec-ch-ua-mobile", "?0");
+			request.Headers.Add("x-instana-l", "1,correlationType=web;correlationId=a72df02c74bb0d7b");
+			request.Headers.Add("accept", "application/json, text/plain, */*");
+			request.Headers.Add("x-requested-with", "XMLHttpRequest");
+			request.Headers.Add("x-session-id", "b30d529c-4c4f-4a1c-bf36-c15a7eac9151");
+			request.Headers.Add("user-agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36");
+			request.Headers.Add("x-country-code", "pl");
+			request.Headers.Add("sec-ch-ua", "\"Google Chrome\";v=\"95\", \"Chromium\";v=\"95\", \";Not A Brand\";v=\"99\"");
+			request.Headers.Add("sec-ch-ua-platform", "\"Linux\"");
+			request.Headers.Add("origin", "https://www.pyszne.pl");
+			request.Headers.Add("sec-fetch-site", "cross-site");
+			request.Headers.Add("sec-fetch-mode", "cors");
+			request.Headers.Add("sec-fetch-dest", "empty");
 			using HttpWebResponse response = (HttpWebResponse) request.GetResponse();
 			using Stream responseStream = response.GetResponseStream();
 			using StreamReader responseReader = new(responseStream);
