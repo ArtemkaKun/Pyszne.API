79708c27b23944492e3c67b3e881a5d6f8d5e19b
Artem Yurchenko <zumikq78@gmail.com>
2021-11-26 21:55:17 +0100

    RestaurantTests class was created, 2 tests were prepared

---
+++ Pyszne-API-lib-server/RestaurantTests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace Pyszne_API_lib_server
+{
+	public class RestaurantTests
+	{
+		[Test]
+		public void SendRestaurantDataRequest_ReturnedCodeIsSuccess_True ()
+		{
+			Assert.Fail();
+		}
+		
+		[Test]
+		public void SendRestaurantDataRequest_ReturnedDataContainsExpectedKeywords_True ()
+		{
+			Assert.Fail();
+		}
+	}
+}
--- Pyszne-API-lib-server/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-using NUnit.Framework;
-
-namespace Pyszne_API_lib_server
-{
-	public class Tests
-	{
-		
-	}
-}
