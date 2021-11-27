3e0e555d91835f17f9fc9659c82c579bcf4e98e1
Artem Yurchenko <zumikq78@gmail.com>
2021-11-27 22:38:36 +0100

    RestaurantDataManager class was refactored

--- PyszneAPILib/RestaurantDataSystem/RestaurantDataManager.cs
+++ PyszneAPILib/RestaurantDataSystem/RestaurantDataManager.cs
@@ -4,11 +4,11 @@ namespace PyszneAPILib.RestaurantDataSystem
 {
 	public class RestaurantDataManager : IRestaurantDataManager
 	{
-		private const string SERVICE_ADDRESS = "https://cw-api.takeaway.com/api/v28/";
+		private const string RESTAURANT_DATA_REQUEST_TEMPLATE = RequestRelatedConstants.DEFAULT_SERVICE_ADDRESS + "restaurant?slug={0}";
 		
 		public HttpRequestMessage CreateRequest (string restaurantID)
 		{
-			string restaurantRequest = $"{SERVICE_ADDRESS}restaurant?slug={restaurantID}";
+			string restaurantRequest = string.Format(RESTAURANT_DATA_REQUEST_TEMPLATE, restaurantID);
 			return new HttpRequestMessage(HttpMethod.Get, restaurantRequest);
 		}
 	}
